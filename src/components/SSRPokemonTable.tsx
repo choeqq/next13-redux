33d7a8bfa93e985a01ff43c438c090f17614dad6
choeqq <k.chola@op.pl>
2023-03-15 20:10:52 +0100

    redux for ssr

--- src/components/SSRPokemonTable.tsx
+++ src/components/SSRPokemonTable.tsx
@@ -1,7 +1,8 @@
 import PokemonTable from "./PokemonTable";
+import { store } from "@/store";
 
 function SSRPokemonTable() {
-  return <PokemonTable pokemons={[]} />;
+  return <PokemonTable pokemons={store.getState().search.startupPokemon} />;
 }
 
 export default SSRPokemonTable;
